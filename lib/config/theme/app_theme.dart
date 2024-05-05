a03a1f61784f9c343e125126c4b67df1b49697fd
Edwin Cardenas <eacardenase@gmail.com>
2024-05-04 21:03:45 -0500

    feat: add presentation and config setup

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData getTheme() {
+    const seedColor = Colors.deepPurple;
+
+    return ThemeData(
+      colorSchemeSeed: seedColor,
+      listTileTheme: const ListTileThemeData(
+        iconColor: seedColor,
+      ),
+      appBarTheme: const AppBarTheme(
+        centerTitle: true,
+        iconTheme: IconThemeData(
+          color: seedColor,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:go_router/go_router.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('Cubits'),
+            subtitle: const Text('State management made simple'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () => context.push('/cubits'),
+          ),
+          ListTile(
+            title: const Text('BLoC'),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
