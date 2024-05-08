006c83e127e8caf882d59a2e5aa93481eced6b48
Edwin Cardenas <eacardenase@gmail.com>
2024-05-08 17:56:22 -0500

    feat: add equatable to prevent rerenders if states are same

--- lib/presentation/screens/cubit_counter_screen.dart
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -25,11 +25,11 @@ class _CubitCounterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final counterState = context.watch<CounterCubit>().state;
+    final counterState = context.watch<CounterCubit>().state;
 
     return Scaffold(
       appBar: AppBar(
-        // title: Text('Cubit Counter: ${counterState.transactionCount}'),
+        title: Text('Cubit Counter: ${counterState.transactionCount}'),
         actions: [
           IconButton(
             onPressed: () => context.read<CounterCubit>().reset(),
@@ -39,7 +39,7 @@ class _CubitCounterView extends StatelessWidget {
       ),
       body: Center(
         child: BlocBuilder<CounterCubit, CounterState>(
-          buildWhen: (previous, current) => current.counter != previous.counter,
+          // buildWhen: (previous, current) => current.counter != previous.counter,
           builder: (context, state) {
             print('DEBUG: Counter has changed');
 
