8fb3baf1028528ea75c7e70f6124d5c93e1ded7f
Edwin Cardenas <eacardenase@gmail.com>
2024-05-22 15:18:10 -0500

    feat: setup counter bloc

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -18,6 +18,7 @@ class HomeScreen extends StatelessWidget {
           ),
           ListTile(
             title: const Text('BLoC'),
+            subtitle: const Text('Robust state management'),
             trailing: const Icon(Icons.arrow_forward_ios),
             onTap: () => context.push('/bloc'),
           ),
